f8ea2d710ea0281c2141b62218b8675ab097aec6
bowd <bogdan@bowd.io>
2022-06-26 15:31:37 +0200

    WIP - actor and deal for celo

--- src/Test.sol
+++ src/Test.sol
@@ -11,4 +11,19 @@ contract Test is ForgeTest, Precompiles {
     constructor() ForgeTest() public {
         ph = new PrecompileHandler();
     }
+
+    function actor(string memory name) public returns (address) {
+      uint256 pk = uint256(keccak256(bytes(name)));
+      address addr = vm.addr(pk);
+      vm.label(addr, name);
+      return addr;
+    }
+
+    //function deal(address token, address to, uint256 give, bool adjust) public {
+    //  (, bytes memory symbolData) = token.call(0x1f1b7586);
+    //  if (keccak256(symbolData) == ) {
+    //  } else {
+    //    super.deal(token, to, give, adjust);
+    //  }
+    //}
 }
