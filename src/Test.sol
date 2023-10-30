26ebfdb505ccdd48591992a2739a6e6486e73626
pahor167 <47992132+pahor167@users.noreply.github.com>
2023-10-30 13:32:52 +0100

    test extended

--- src/Test.sol
+++ src/Test.sol
@@ -1,56 +1,91 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.5.13;
 
-import {Test as ForgeTest} from "forge-std/Test.sol";
+import { Test as ForgeTest } from "forge-std/Test.sol";
 import "./Precompiles.sol";
 import "./PrecompileHandler.sol";
 
 contract Test is ForgeTest, Precompiles {
-    PrecompileHandler public ph;
-    address currentPrank;
+  PrecompileHandler public ph;
+  address currentPrank;
 
-    event log_named_array(string key, address[] val);
-    event log_named_array(string key, bytes32[] val);
-    event log_array(string key, address[] val);
-    event log_array(string key, bytes32[] val);
+  event log_named_array(string key, address[] val);
+  event log_named_array(string key, bytes32[] val);
+  event log_array(string key, address[] val);
+  event log_array(string key, bytes32[] val);
 
-    constructor() ForgeTest() public {
-        ph = new PrecompileHandler();
+  constructor() public ForgeTest() {
+    ph = new PrecompileHandler();
+  }
+
+  /* Utility functions */
+
+  function changePrank(address who) internal {
+    // Record current prank so helper functions can revert
+    // if they need to prank
+    currentPrank = who;
+    super.changePrank(who);
+  }
+
+  function actor(string memory name) public returns (address) {
+    uint256 pk = uint256(keccak256(bytes(name)));
+    address addr = vm.addr(pk);
+    vm.label(addr, name);
+    return addr;
+  }
+
+  function actorWithPK(string memory name) public returns (address, uint256) {
+    uint256 pk = uint256(keccak256(bytes(name)));
+    address addr = vm.addr(pk);
+    vm.label(addr, name);
+    return (addr, pk);
+  }
+
+  /* Extra assertions, extends forge-std/Test.sol */
+
+  function assertEq(address[] memory a, address[] memory b) internal {
+    if (keccak256(abi.encode(a)) != keccak256(abi.encode(b))) {
+      emit log("Error: a == b not satisfied [address[]]");
+      emit log_named_array("  Expected", b);
+      emit log_named_array("    Actual", a);
+      fail();
     }
+  }
 
-    /* Utility functions */
-
-    function changePrank(address who) internal {
-      // Record current prank so helper functions can revert
-      // if they need to prank
-      currentPrank = who;
-      super.changePrank(who);
+  function assertEq(address[] memory a, address[] memory b, string memory err) internal {
+    if (keccak256(abi.encode(a)) != keccak256(abi.encode(b))) {
+      emit log_named_string("Error", err);
+      assertEq(a, b);
     }
+  }
 
-    function actor(string memory name) public returns (address) {
-      uint256 pk = uint256(keccak256(bytes(name)));
-      address addr = vm.addr(pk);
-      vm.label(addr, name);
-      return addr;
+  function assertEq(bytes32[] memory a, bytes32[] memory b) internal {
+    if (keccak256(abi.encode(a)) != keccak256(abi.encode(b))) {
+      emit log("Error: a == b not satisfied [bytes32[]]");
+      emit log_named_array("  Expected", b);
+      emit log_named_array("    Actual", a);
+      fail();
     }
+  }
 
-    /* Extra assertions, extends forge-std/Test.sol */
+  function deployCodeTo(string memory what, address where) internal {
+    deployCodeTo(what, "", 0, where);
+  }
 
-    function assertEq(address[] memory a, address[] memory b) internal {
-        if (keccak256(abi.encode(a)) != keccak256(abi.encode(b))) {
-            emit log("Error: a == b not satisfied [address[]]");
-            emit log_named_array("  Expected", b);
-            emit log_named_array("    Actual", a);
-            fail();
-        }
-    }
+  function deployCodeTo(string memory what, bytes memory args, address where) internal {
+    deployCodeTo(what, args, 0, where);
+  }
 
-    function assertEq(bytes32[] memory a, bytes32[] memory b) internal {
-        if (keccak256(abi.encode(a)) != keccak256(abi.encode(b))) {
-            emit log("Error: a == b not satisfied [bytes32[]]");
-            emit log_named_array("  Expected", b);
-            emit log_named_array("    Actual", a);
-            fail();
-        }
-    }
+  function deployCodeTo(string memory what, bytes memory args, uint256 value, address where)
+    internal
+  {
+    bytes memory creationCode = vm.getCode(what);
+    vm.etch(where, abi.encodePacked(creationCode, args));
+    (bool success, bytes memory runtimeBytecode) = where.call.value(value)("");
+    require(
+      success,
+      "StdCheats deployCodeTo(string,bytes,uint256,address): Failed to create runtime bytecode."
+    );
+    vm.etch(where, runtimeBytecode);
+  }
 }
